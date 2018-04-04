3d067c57aed26c792ae931346f3f2f60af6f7c6c
davidelasi <davide.lasi@gmail.com>
2018-04-04 10:31:21 -0400

    first commit

--- exercise.js
+++ exercise.js
@@ -2,26 +2,28 @@ var exercise = {};
 
 // calculate distance between points
 exercise.distance = function(p1,p2){
-    //-------------------
-    //---- Your Code ----
-    //-------------------    
-    return 'Error: 1st function not implemented';    
+    var dx = p1.x-p2.x;
+    var dy = p1.y-p2.y;
+    return Math.sqrt(dx*dy+dy*dy);    
+
 };
 
 // calculate distance between new point and existing
 exercise.distances = function(newPoint,data){
-    //-------------------
-    //---- Your Code ----
-    //-------------------
-    return 'Error: 2nd function not implemented';
+    data.forEach(function(existingPoint){
+        var dx = existingPoint.x - newPoint.x;
+        var dy = existingPoint.y - newPoint.y;
+        existingPoint.distance = Math.sqrt(dx * dx + dy * dy);
+    });
+    return data;
 };
 
 // find k closest points
 exercise.findClosest = function(k,data){
-    //-------------------
-    //---- Your Code ----
-    //------------------- 
-    return 'Error: 3rd function not implemented';    
+    data.sort(function(a,b){
+        return a.distance - b.distance;
+    });
+    return data.slice(0,k);    
 };
 
 module.exports = exercise;
